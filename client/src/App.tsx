5b7d546620d6557054a43e94680482fb42915ede
ajr <reanojohn@gmail.com>
2025-08-14 21:26:22 +1000

    feat: add metrics route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,17 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 
 import Home from "./pages/Home";
 import MainLayout from "./layouts/MainLayout";
+import Metrics from "./pages/Metrics";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout />,
-      children: [{ path: "/", element: <Home /> }],
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/metrics", element: <Metrics /> },
+      ],
     },
   ]);
   return <RouterProvider router={router} />;
